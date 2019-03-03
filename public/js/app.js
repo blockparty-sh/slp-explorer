06f197c3efef48cf28273644563a90f02ae05154
blockparty <hello@blockparty.sh>
2019-03-02 18:37:08 -0600

    refactor 2

--- public/js/app.js
+++ public/js/app.js
@@ -93,6 +93,120 @@ app.slpdb = {
   }),
 };
 
+
+
+
+app.template = {
+  token_details: ejs.compile(`
+    <div class="table-responsive">
+      <table class="table table-striped table-sm" id="token-details-table">
+        <thead class="thead-dark">
+          <tr>
+            <th>token id</th>
+            <th>name</th>
+            <th>symbol</th>
+            <th>timestamp</th>
+            <th>quantity</th>
+            <th>decimals</th>
+            <th>document uri</th>
+            <th>document sha256hex</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="/token#<%= tokenIdHex %>"><%= tokenIdHex %></a></td>
+            <td><%= name %></td>
+            <td><%= symbol %></td>
+            <td><%= timestamp %></td>
+            <td><%= genesisOrMintQuantity %></td>
+            <td><%= decimals %></td>
+            <td><a href="<%= documentUri %>"><%= documentUri %></a></td>
+            <td><%= documentSha256Hex %></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `),
+
+  token_stats: ejs.compile(`
+    <div class="table-responsive">
+      <table class="table table-striped table-sm" id="token-stats-table">
+        <thead class="thead-dark">
+          <tr>
+            <th>block created</th>
+            <th>block last active send</th>
+            <th>block last active mint</th>
+            <th>qty valid txns since genesis</th>
+            <th>qty valid token utxos</th>
+            <th>qty valid token addresses</th>
+            <th>qty token minted</th>
+            <th>qty token burned</th>
+            <th>qty token circulating supply</th>
+            <th>qty satoshis locked up</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><%= block_created %></td>
+            <td><%= block_last_active_send %></td>
+            <td><%= block_last_active_mint %></td>
+            <td><%= qty_valid_txns_since_genesis %></td>
+            <td><%= qty_valid_token_utxos %></td>
+            <td><%= qty_valid_token_addresses %></td>
+            <td><%= qty_token_minted %></td>
+            <td><%= qty_token_burned %></td>
+            <td><%= qty_token_circulating_supply %></td>
+            <td><%= qty_satoshis_locked_up %></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `),
+};
+
+
+
+
+app.init_all_tokens_page = () => {
+  const template = ejs.compile(`
+    <div class="table-responsive">
+      <table class="table table-striped table-sm" id="tokens-table">
+        <thead class="thead-dark">
+          <tr>
+            <th>tokenId</th>
+            <th>name</th>
+            <th>symbol</th>
+            <th>circulatingSupply</th>
+            <th>burnedQty</th>
+            <th>mintedQty</th>
+          </tr>
+        </thead>
+        <tbody>
+        <% for (let m of t) { %>
+          <tr>
+            <td><a href="/token#<%= m.tokenId %>"><%= m.tokenId %></a></td>
+            <td><%= m.name %></td>
+            <td><%= m.symbol %></td>
+            <td><%= m.circulatingSupply %></td>
+            <td><%= m.burnedQty %></td>
+            <td><%= m.mintedQty %></td>
+          </tr>
+        <% } %>
+        </tbody>
+      </table>
+    </div>
+  `);
+  
+  const tokens_table_container_el = document.getElementById('tokens-table-container');
+  
+  app.slpdb.query(app.slpdb.all_tokens())
+  .then((data) => {
+    tokens_table_container_el.insertAdjacentHTML('beforeend', template(data));
+  });
+};
+
+
+
 app.init_tx_page = (txid) => {
   const tx_template = ejs.compile(`
     <tr>
@@ -141,79 +255,33 @@ app.init_tx_page = (txid) => {
   `);
   const tx_table_el = document.getElementById('tx-table');
 
-
-  const token_details_template = ejs.compile(`
-    <tr>
-      <td><a href="/token#<%= tokenIdHex %>"><%= tokenIdHex %></a></td>
-      <td><%= name %></td>
-      <td><%= symbol %></td>
-      <td><%= timestamp %></td>
-      <td><%= genesisOrMintQuantity %></td>
-      <td><%= decimals %></td>
-      <td><a href="<%= documentUri %>"><%= documentUri %></a></td>
-      <td><%= documentSha256Hex %></td>
-    </tr>
-  `);
-  const token_details_table_el = document.getElementById('token-details-table');
-
-
+  const token_details_table_container_el = document.getElementById('token-details-table-container');
 
   app.slpdb.query(app.slpdb.tx(txid))
   .then((data) => {
-    console.log(data);
-    data.u.forEach((o) => {
-      tx_table_el.insertAdjacentHTML('beforeend', tx_template(o));
+    const tmp = (tx) => {
+      tx_table_el.insertAdjacentHTML('beforeend', tx_template(tx));
 
-      app.slpdb.query(app.slpdb.token(o.tokenIdHex))
+      app.slpdb.query(app.slpdb.token(tx.tokenIdHex))
       .then((data) => {
-        data.t.forEach((o) => {
-          token_details_table_el.insertAdjacentHTML('beforeend', token_details_template(o.tokenDetails));
-        });
+        if (data.t.length > 0) {
+          token_details_table_container_el
+          .insertAdjacentHTML('beforeend', app.template.token_details(data.t[0].tokenDetails));
+        } else {
+          window.alert('token not found');
+        }
       });
-    });
-    data.c.forEach((o) => {
-      tx_table_el.insertAdjacentHTML('beforeend', tx_template(o));
+    };
 
-      app.slpdb.query(app.slpdb.token(o.tokenIdHex))
-      .then((data) => {
-        data.t.forEach((o) => {
-          token_details_table_el.insertAdjacentHTML('beforeend', token_details_template(o.tokenDetails));
-        });
-      });
-    });
+         if (data.u.length > 0) tmp(data.u[0]);
+    else if (data.c.length > 0) tmp(data.c[0]);
   });
 };
 
 app.init_token_page = (tokenIdHex) => {
-  const token_stats_template = ejs.compile(`
-    <tr>
-      <td><%= block_created %></td>
-      <td><%= block_last_active_send %></td>
-      <td><%= block_last_active_mint %></td>
-      <td><%= qty_valid_txns_since_genesis %></td>
-      <td><%= qty_valid_token_utxos %></td>
-      <td><%= qty_valid_token_addresses %></td>
-      <td><%= qty_token_minted %></td>
-      <td><%= qty_token_burned %></td>
-      <td><%= qty_token_circulating_supply %></td>
-      <td><%= qty_satoshis_locked_up %></td>
-    </tr>
-  `);
-  const token_stats_table_el = document.getElementById('token-stats-table');
+  const token_stats_table_container_el = document.getElementById('token-stats-table-container');
   
-  const token_details_template = ejs.compile(`
-    <tr>
-      <td><%= tokenIdHex %></td>
-      <td><%= name %></td>
-      <td><%= symbol %></td>
-      <td><%= timestamp %></td>
-      <td><%= genesisOrMintQuantity %></td>
-      <td><%= decimals %></td>
-      <td><a href="<%= documentUri %>"><%= documentUri %></a></td>
-      <td><%= documentSha256Hex %></td>
-    </tr>
-  `);
-  const token_details_table_el = document.getElementById('token-details-table');
+  const token_details_table_container_el = document.getElementById('token-details-table-container');
   
   
   const token_address_template = ejs.compile(`
@@ -238,18 +306,22 @@ app.init_token_page = (tokenIdHex) => {
   
   app.slpdb.query(app.slpdb.token(tokenIdHex))
   .then((data) => {
-    data.t.forEach((o) => {
-      token_details_table_el.insertAdjacentHTML('beforeend', token_details_template(o.tokenDetails));
-      token_stats_table_el.insertAdjacentHTML('beforeend', token_stats_template(o.tokenStats));
+    if (data.t.length > 0) {
+      token_details_table_container_el
+      .insertAdjacentHTML('beforeend', app.template.token_details(data.t[0].tokenDetails));
+
+      token_stats_table_container_el
+      .insertAdjacentHTML('beforeend', app.template.token_stats(data.t[0].tokenStats));
   
-      o.addresses.forEach((v) => {
-        token_address_table_el.insertAdjacentHTML('beforeend', token_address_template({
+      data.t[0].addresses.forEach((v) => {
+        token_address_table_el
+        .insertAdjacentHTML('beforeend', token_address_template({
           address: v[0],
           balance_satoshis: v[1]['bch_balance_satoshis'],
           balance_tokens:   v[1]['token_balance']
   	  })
       )});
-    });
+    }
   });
   
   app.slpdb.query(app.slpdb.token_transaction_history(tokenIdHex))
@@ -263,25 +335,4 @@ app.init_token_page = (tokenIdHex) => {
   });
 };
 
-app.init_all_tokens_page = () => {
-  const template = ejs.compile(`
-    <tr>
-      <td><a href="/token#<%= tokenId %>"><%= tokenId %></a></td>
-      <td><%= name %></td>
-      <td><%= symbol %></td>
-      <td><%= circulatingSupply %></td>
-      <td><%= burnedQty %></td>
-      <td><%= mintedQty %></td>
-    </tr>
-  `);
-  
-  const tokens_table_el = document.getElementById('tokens-table');
-  
-  app.slpdb.query(app.slpdb.all_tokens())
-  .then((data) => {
-      data.t.forEach((o) => {
-        tokens_table_el.insertAdjacentHTML('beforeend', template(o));
-      });
-  });
-};
   
