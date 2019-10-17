896a88c102c3bf59a95a7fab6eba75418ebdc178
blockparty <hello@blockparty.sh>
2019-10-17 14:36:49 -0500

    link to address from rich list

--- js/app.js
+++ js/app.js
@@ -3046,7 +3046,10 @@ app.init_token_page = (tokenIdHex) =>
 
         try {
           Plotly.newPlot('plot-token-address-rich', [{
-            x: data.map(v => v.address),
+            x: data.map(v => (v.address !== 'Other')
+              ? `<a href="/#address/${v.address}">${v.address}</a>`
+              : v.address
+            ),
             y: data.map(v => v.token_balance),
             marker: {
               color: data.map(v => v.color)
